d0875707574b280a13a56711d46d5cd97d428ae1
DattaDhebe <dattadhebe75@gmail.com>
2020-04-21 12:10:30 +0530

    Added.[useCase1] initialized board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,1 +1,18 @@
 #!/bin/bash -x
+
+noOfRows=3;
+noOfColumns=3;
+initSymbol="-"
+
+initBoard() {
+	for (( row=1; row<=noOfRows; row++ ))
+	do
+		for (( column=0; column<=noOfRows; column++))
+		do
+			board[$row, $column]=$initSysmbol
+		done
+	done
+}
+
+			
+
