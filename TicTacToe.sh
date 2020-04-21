d2f06c98bbc2cd932e9d10fbab347b5badbcdc12
DattaDhebe <dattadhebe75@gmail.com>
2020-04-21 21:54:54 +0530

    Refactor. initialize position and Display Board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,20 +1,31 @@
-#!/bin/bash -x
+#/bin/bash -x
 
 noOfRows=3;
 noOfColumns=3;
-initSymbol="-"
+placeHolder="-";
+declare -A gameBoard
 
 function initBoard() {
 	for (( row=1; row<=noOfRows; row++ ))
 	do
-		for (( column=0; column<=noOfRows; column++))
+		for (( column=1; column<=noOfRows; column++))
 		do
-			board[$row, $column]=$initSysmbol
+			gameBoard[$row, $column]=$placeHolder
 		done
 	done
 }
-
-
+function printBoard() {
+	for (( row=1; row<=noOfRows; row++))
+	do
+		for ((column=1; column<=noOfColumns; column++))
+		do
+			printf "| ${gameBoard[$row, $column]} "
+		done
+		printf "|\n"
+	done
+}
 
 			
 
+initBoard
+printBoard
